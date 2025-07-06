908dccf94ae30cbcc5d658b2dca32957b8df6668
Ashadowmonarch <forcoding39@gmail.com>
2025-07-05 23:31:11 -0400

    add pseudo branching

---
+++ pseudo_branch.asm
@@ -0,0 +1,21 @@
+# Pseudo-instruction
+
+.data
+	message: .asciiz "Hi, how are you?"
+	
+.text
+	main:
+		addi $s0, $zero, 14
+		addi $s1, $zero, 10
+		
+		bgt $s0, $s1, displayHi
+		# blt (opposit bgt)
+		#bgtz
+	#End of main
+	li $v0, 10
+	syscall
+	
+	displayHi:
+		li $v0, 4
+		la $a0, message
+		syscall
